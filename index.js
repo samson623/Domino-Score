79c141d6b6b6281f946c88ee935b2908204cc00c
samson623 <1samson623@gmail.com>
2025-08-26 13:58:21 -0700

    Fix start script and convert server to ESM

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-require('dotenv').config();
+import { config } from 'dotenv';
+import { createClient } from '@supabase/supabase-js';
 
-const { createClient } = require('@supabase/supabase-js');
+config();
 
 const supabase = createClient(
-process.env.NEXT_PUBLIC_SUPABASE_URL,
-process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
 
 console.log('Supabase client initialized successfully');
